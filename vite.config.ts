f6311460962dffaf4f2570f2c24170bf340d3f68
dpieve <26913187+dpieve@users.noreply.github.com>
2026-02-27 21:09:23 -0300

    create workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/invoice-generator/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
